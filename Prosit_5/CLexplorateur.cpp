f722d281a2dcf0602805d40473d6a4541057670b
Peio Rigaud <peio.rigaud@viacesi.fr>
2023-11-24 09:02:50 +0100

    Rajout de toute les classes

---
+++ Prosit_5/CLexplorateur.cpp
@@ -0,0 +1,1 @@
+#include "CLexplorateur.h"
--- Prosit_5/MyForm.h
+++ Prosit_5/MyForm.h
@@ -48,11 +48,17 @@ namespace Prosit5 {
 		/// </summary>
 		void InitializeComponent(void)
 		{
-			this->components = gcnew System::ComponentModel::Container();
-			this->Size = System::Drawing::Size(300,300);
-			this->Text = L"MyForm";
-			this->Padding = System::Windows::Forms::Padding(0);
+			this->SuspendLayout();
+			// 
+			// MyForm
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(316, 293);
+			this->Name = L"MyForm";
+			this->Text = L"MyForm";
+			this->ResumeLayout(false);
+
 		}
 #pragma endregion
 	};
